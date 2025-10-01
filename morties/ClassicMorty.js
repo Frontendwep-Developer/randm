b2b32a82d13ef2d6830d10c02a89b98e4c73c7ef
Dilshodbek48 <xolmominovdilshodbek8888@gmail.com>
2025-10-01 17:26:22 +0500

    Add some changes

--- morties/ClassicMorty.js
+++ morties/ClassicMorty.js
@@ -7,25 +7,20 @@ export class ClassicMorty extends BaseMorty {
     }
 
     async removeBoxes(selectedBox, portalGunBox, randomGenerator) {
-        // Classic Morty portal quroli bo'lgan qutini hech qachon olib tashlamaydi
+
         const remainingBoxes = [selectedBox];
 
-        // Qolgan qutilardan tasodifiy tanlash
         const otherBoxes = Array.from({length: this.numBoxes}, (_, i) => i)
             .filter(box => box !== selectedBox);
 
-        // Agar portal quroli tanlanmagan bo'lsa, uni saqlab qolish
         if (!otherBoxes.includes(portalGunBox)) {
             otherBoxes.push(portalGunBox);
         }
 
-        // ⚠️ YANGI: Rickdan so'ramaymiz, o'zimiz tasodifiy yaratamiz
-        // Ikkinchi HMAC ni yaratish
         const hmac2 = randomGenerator.generateMortyValue(otherBoxes.length);
         console.log(`Morty: HMAC2 = ${hmac2}`);
         console.log(`Morty: I'm generating a fair random number to decide which box to keep...`);
 
-        // Morty o'zi tasodifiy raqam yaratadi (Rick kiritishi shart emas)
         const mortyRandomValue = Math.floor(Math.random() * otherBoxes.length);
         const keptBoxIndex = randomGenerator.getFinalValue(mortyRandomValue, otherBoxes.length);
         const keptBox = otherBoxes[keptBoxIndex];
@@ -37,7 +32,6 @@ export class ClassicMorty extends BaseMorty {
     }
 
     calculateProbability(didSwitch) {
-        // Classic Monty Hall ehtimoli
         if (didSwitch) {
             return (this.numBoxes - 1) / this.numBoxes;
         } else {
